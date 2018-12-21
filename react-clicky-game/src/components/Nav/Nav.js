316bedd05fda4246965426a00684c71df7f00334
memisgulden <memis@gmail.com>
2018-12-20 20:32:20 -0500

    add

---
+++ react-clicky-game/src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => (
+  <nav className="navbar navbar-default navbar-fixed-top">
+    <a className="navbar-brand" href="#">
+      Clicky Game
+    </a>
+    <li>You Guessed</li>
+    <li>Score</li>
+
+    
+  </nav>
+);
+
+export default Nav;
